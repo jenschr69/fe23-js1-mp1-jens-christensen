de7cbf447bc5ede3e9c3b5fd1c8b544214a02bc1
Jens Chr. Christensen <jcc@jenschrchristensen.com>
2023-11-10 10:26:09 +0100

    Added pseudo code

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+/* Miniproject 1 FE2023 Del 1
+1. Skapa 5 "bars" / blockar
+2. Lägg till olika bakgrundsfärger
+3. Lägg till text i bars'en
+4. Lägg till olika storleker som växer från bar till bar
+*/
+
+/* 1. Skapa bars */
+/* Börja med att ange antal rader/bars */
+const barsToCreate = 5;
+
+/* Generera antal div'ar */
+
+/* Skapa olika classes för varje bar*/
+/* Tilldela färgvariabel */ 
+/* */
+
+/* Lägga till klasser på div'er */
+
+/* 2. Add background colors */
+
+const colors = []
+
+/* Miniproject 1 FE2023 Del 2
+1. Skapa 5 "bars" / blockar
+2. Lägg till olika bakgrundsfärger
+4. Lägg till text i bars'en
+5. Lägg till olika storleker som växer från bar till bar
+*/
